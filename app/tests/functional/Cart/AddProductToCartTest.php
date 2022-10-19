9c3d65faf889d3650718ef77ed9862537a41d1ea
Jan Czerwiński <jankacper.czerwinski@gmail.com>
2022-10-19 09:24:35 +0200

    test(cart): add more assertions to AddProductToCartTest

--- app/tests/functional/Cart/AddProductToCartTest.php
+++ app/tests/functional/Cart/AddProductToCartTest.php
@@ -14,11 +14,25 @@ class AddProductToCartTest extends WebTestCase implements FunctionalTestInterfac
     {
         // given
         $client = static::createClient();
-        $productId = ProductTestCase::createProductRequest($client, 'Title', 19900)['productId'];
+        $price1 = 2000;
+        $product1Id = ProductTestCase::createProductRequest($client, 'Title', $price1)['productId'];
+        $price2 = 1500;
+        $product2Id = ProductTestCase::createProductRequest($client, 'Title2', $price2)['productId'];
         $cartId = CartTestCase::createCartRequest($client)['cartId'];
+        $quantity1 = 2;
+        $quantity2 = 3;
 
         // when then
-        $result = CartTestCase::postCartProductRequest($client, $cartId, $productId, 2);
-        dump($result);
+        CartTestCase::postCartProductRequest($client, $cartId, $product1Id, $quantity1);
+        $result = CartTestCase::postCartProductRequest($client, $cartId, $product2Id, $quantity2);
+        $this->assertEquals($cartId, $result['cart']['id']);
+        $this->assertCount(2, $result['cart']['products']);
+        $this->assertEquals($product1Id, $result['cart']['products'][0]['product']['id']);
+        $this->assertEquals($product2Id, $result['cart']['products'][1]['product']['id']);
+        $this->assertEquals($price1, $result['cart']['products'][0]['product']['price']['netPrice']);
+        $this->assertEquals($price2, $result['cart']['products'][1]['product']['price']['netPrice']);
+        $this->assertEquals($quantity1, $result['cart']['products'][0]['quantity']);
+        $this->assertEquals($quantity2, $result['cart']['products'][1]['quantity']);
+        $this->assertEquals(($price1 * $quantity1) + ($price2 * $quantity2), $result['cart']['prices']['totalPriceNet']);
     }
 }
